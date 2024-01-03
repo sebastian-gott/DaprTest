c2c812292a75ce13647eac13156d0ec7fa4d0825
Sebastian Gottling <sebastian.gottling@banenor.no>
2024-01-03 08:37:27 +0100

    pospsps

---
+++ producer/producer/producer/Models/SampleMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace producer.Models
+{
+    public class SampleMessage
+    {
+        public Guid CorrelationId { get; set; }
+        public Guid MessageId { get; set; }
+        public string Message { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Sentiment { get; set; }
+        public DateTime PreviousAppTimestamp { get; set; }
+    }
+}
---
+++ producer/producer/producer/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace producer
+{
+    using Dapr.Client;
+    using producer.Models;
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static async Task GenerateMessagesAsync()
+        {
+            var daprClientBuilder = new DaprClientBuilder();
+            var client = daprClientBuilder.Build();
+
+            while (true)
+            {
+                var message = GenerateNewMessage();
+                Console.WriteLine("Publishing: {0}", message);
+
+                try
+                {
+                    await client.PublishEventAsync<SampleMessage>("sampletopic", "test", message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                // Delay 10 seconds
+                await Task.Delay(TimeSpan.FromSeconds(10.0));
+            }
+        }
+
+        static internal SampleMessage GenerateNewMessage()
+        {
+            return new SampleMessage()
+            {
+                CorrelationId = Guid.NewGuid(),
+                MessageId = Guid.NewGuid(),
+                Message = GenerateRandomMessage(),
+                CreationDate = DateTime.UtcNow,
+                PreviousAppTimestamp = DateTime.UtcNow
+            };
+        }
+
+        static internal string GenerateRandomMessage()
+        {
+            var random = new Random();
+            var HashTags = new string[]
+            {
+                "circle",
+                "ellipse",
+                "square",
+                "rectangle",
+                "triangle",
+                "star",
+                "cardioid",
+                "epicycloid",
+                "limocon",
+                "hypocycoid"
+            };
+
+            int length = random.Next(5, 10);
+            string s = "";
+            for (int i = 0; i < length; i++)
+            {
+                int j = random.Next(26);
+                char c = (char)('a' + j);
+                s += c;
+            }
+            s += " #" + HashTags[random.Next(HashTags.Length)];
+            return s;
+        }
+    }
+}
